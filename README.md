a201be9113806881ada03237f339e20ace184b6e
Florence <monnaie.florence@gmail.com>
2021-03-17 08:54:56 +0100

    left model to change and print data

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Afficher dans la page welcome
 -Une section age l'id, l'email et le name de la personne connecté.
 
 -Une section avec tous les users inscrits -> id,name,email,
+
+
+
+<!-- part 2 -->
+
+Rajouter une table profil avec "prenom", "age" et une clef étrangère user_id
+Dans le register rajouter un champ pour prénom et age
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -51,6 +52,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'first' => ['required', 'string', 'max:255'],
+            'age' => ['required', 'integer', 'between:0,120'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -64,10 +67,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        Profile::create([
+            'first' => $data['first'],
+            'age' => $data['age'],
+            'user_id' => $user->id,
+        ]);
+        return $user;
     }
 }
